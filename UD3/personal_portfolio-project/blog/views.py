a9b117d2a21583ec45f17c6da608f3669b1ba89f
a21alejandrovp <a21alejandrovp@iessanclemente.net>
2024-11-15 12:52:33 +0100

    perezoso

--- UD3/personal_portfolio-project/blog/views.py
+++ UD3/personal_portfolio-project/blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Article
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    articles = Article.objects.order_by('-date')[:3]
+    return render(request, 'blog/all_blogs.html', {'articles':articles})
+
+def detail(request, blog_id):
+    return render(request, 'blog/detail.html', {'id': blog_id})
