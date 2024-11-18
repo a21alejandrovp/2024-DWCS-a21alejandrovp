44e85194402bea045944831092535f6396b52c3d
a21alejandrovp <a21alejandrovp@iessanclemente.net>
2024-11-18 17:36:28 +0100

    modificado directorio personal_portfolio-project

--- UD3/personal_portfolio-project/blog/views.py
+++ UD3/personal_portfolio-project/blog/views.py
@@ -5,5 +5,6 @@ def all_blogs(request):
     articles = Article.objects.order_by('-date')[:3]
     return render(request, 'blog/all_blogs.html', {'articles':articles})
 
-def detail(request, blog_id):
-    return render(request, 'blog/detail.html', {'id': blog_id})
+def detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'blog/detail.html', {'id': article_id})
