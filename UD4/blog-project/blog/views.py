126df42e5a50366156f4751c1a6b5edef427d106
a21alejandrovp <a21alejandrovp@iessanclemente.net>
2025-01-22 11:08:31 +0100

    modificado proyecto DJANGO (blog-project)

--- UD4/blog-project/blog/views.py
+++ UD4/blog-project/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.order_by('date')[:3]
+    return render (request, "blog/index.html", {'posts':posts})
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/detail.html', {'post':post})
